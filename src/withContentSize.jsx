5a539c57432be9d8cda05fbf86dbd78cc54c6cf1
Esbjörn Blomquist <esbjorn.blomquist@cybercom.com>
2017-10-30 11:05:09 +0100

    proptypes

--- src/withContentSize.jsx
+++ src/withContentSize.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import muiThemeable from 'material-ui/styles/muiThemeable';
 import { connect } from 'react-redux';
 // import { large } from '../styles/responsiveness';
